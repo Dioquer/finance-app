9e7e77c60bf88bcd2bf4bc67f2558038fc951d04
Dioquer <alcpmantonio@gmail.com>
2025-06-29 03:22:55 -0300

    Inicialização do projeto com estrutura base.

---
+++ src/main/java/com/antonio/finance_app/FinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.antonio.finance_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceAppApplication.class, args);
+	}
+
+}
